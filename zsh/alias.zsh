0f51d50ff76d2503e7f904b4605a6154f01b4e08
Johannes Schwarzbart <schwarzbartj@gmail.com>
2020-11-29 00:30:57 +0100

    add zsh configurations

---
+++ zsh/alias.zsh
@@ -0,0 +1,32 @@
+# Git
+alias g="git"
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
+# Convenience
+alias mkdir="mkdir -p"
+# ls, the common ones I use a lot shortened for rapid fire usage
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Rust
+alias cr="cargo run"
+alias cc="cargo check"
+alias ct="cargo test"
---
+++ zsh/zshrc.zsh
@@ -0,0 +1,40 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+export PATH="/usr/local/bin:$PATH"
+
+source ~/.alias
+source ~/antigen.zsh
+source ~/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+source ~/.bash_profile
+
+#alias daily_lookup="cd ~/Dropbox/KnowledgeBase; rg -t md -l '\[\#DailyLookup\]' | gshuf -n 1 | xargs -I{} open {}"
+
+# Antigen Settings
+# Load the theme.
+#antigen theme robbyrussell
+antigen bundle z
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
+antigen theme romkatv/powerlevel10k
+
+
+#antigen bundle mafredri/zsh-async
+#antigen bundle sindresorhus/pure
+#
+# Tell Antigen that you're done.
+antigen apply
+
+# zsh-history-substring-search configuration
+# (put whatever configuration you want here!)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
