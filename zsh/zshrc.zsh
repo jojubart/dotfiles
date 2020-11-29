e822aef2f2c4de660e4f346d6f44ad41dd46275e
Johannes Schwarzbart <schwarzbartj@gmail.com>
2020-11-29 12:07:00 +0100

    change default editor to neovim

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,6 +6,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 export PATH="/usr/local/bin:$PATH"
+export EDITOR=nvim
 
 source ~/.alias
 source ~/antigen.zsh
